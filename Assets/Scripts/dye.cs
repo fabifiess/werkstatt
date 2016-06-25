4fca531f9eb04ac06ddc945a87538290359ab606
Fab F <MXO-GFX@Maximilians-iMac-2.local>
2016-06-25 12:14:06 +0200

    Artnetzend Intergration

---
+++ Assets/Scripts/dye.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using ArtNet;
+
+public class dye : MonoBehaviour {
+
+	public List<GameObject> climbingholds = new List<GameObject>();
+
+	int toggle = 0; 
+	// private float[] hue_A = {1.0f, 0.0f, 0.0f};
+	private float[] hue_B = {0.5f, 1.0f, 1.0f};
+	private float[] hue_C = {1.0f, 0.0f, 1.0f};
+
+	ArtNet.Engine ArtEngine; 
+
+	void Start () {
+		// Artnet sender / client
+		ArtEngine = new ArtNet.Engine("Open DMX Etheret", "192.168.0.90");
+		ArtEngine.Start ();
+
+		// climbingholds[0].GetComponent<Renderer>().material.color = Color.magenta;
+	}
+
+	void Update () {
+
+		// get position of stone
+		// Vector3 stonePos = new Vector3 (climbingholds [1].transform.position.x, climbingholds [1].transform.position.y, climbingholds [1].transform.position.z);
+		// Debug.Log ("x: " + stonePos.x + ", y: " + stonePos.y + ", z: " + stonePos.z);
+		// get the color of the Movie texture at that position
+		// render this color to the virtual stone
+
+
+
+
+		if (Input.GetKeyDown("c")){
+			toggle++;
+
+			// fade to value fadeTo(aValue, aTime);
+			if (toggle % 2 == 1) {
+				StartCoroutine (FadeRender (0, hue_C));
+				StartCoroutine (FadeDmx (0, hue_C));
+			} else {
+				StartCoroutine (FadeRender (0, hue_B));
+				StartCoroutine (FadeDmx (0, hue_B));
+			}
+		}
+
+		if (Input.GetKeyDown("b")){
+			toggle++;
+
+			// fade to value fadeTo(aValue, aTime);
+			if (toggle % 2 == 1) {
+				StartCoroutine (FadeRender (1, hue_C));
+				StartCoroutine (FadeDmx (1, hue_C));
+			} else {
+				StartCoroutine (FadeRender (1, hue_B));
+				StartCoroutine (FadeDmx (1, hue_B));
+			}
+		}
+	}
+
+
+
+	IEnumerator FadeDmx(int hold_nr, float [] fadeToColor){
+		float fadingTime = 2.0f;
+		float prevRed = climbingholds[hold_nr].GetComponent<Renderer> ().material.color.r;
+		float prevGreen = climbingholds[hold_nr].GetComponent<Renderer> ().material.color.g;
+		float prevBlue = climbingholds[hold_nr].GetComponent<Renderer> ().material.color.b;
+
+		for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / fadingTime){
+
+			byte[] DMXData = new byte[512];
+
+			DMXData [hold_nr * 3 + 0] = (byte)(Mathf.Lerp(prevRed, fadeToColor[0], t)*255);
+			DMXData [hold_nr * 3 + 1] = (byte)(Mathf.Lerp(prevGreen, fadeToColor[1], t)*255);
+			DMXData [hold_nr * 3 + 2] = (byte)(Mathf.Lerp(prevBlue, fadeToColor[2], t)*255);
+
+			ArtEngine.SendDMX (0, DMXData, DMXData.Length);
+
+			yield return null;
+		}
+	}
+
+	IEnumerator FadeRender(int hold_nr, float [] fadeToColor){
+		float fadingTime = 2.0f;
+		float prevRed = climbingholds[hold_nr].GetComponent<Renderer> ().material.color.r;
+		float prevGreen = climbingholds[hold_nr].GetComponent<Renderer> ().material.color.g;
+		float prevBlue = climbingholds[hold_nr].GetComponent<Renderer> ().material.color.b;
+
+		for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / fadingTime){
+			Color newColor = new Color(
+				Mathf.Lerp(prevRed, fadeToColor[0], t),
+				Mathf.Lerp(prevGreen, fadeToColor[1], t),
+				Mathf.Lerp(prevBlue, fadeToColor[2], t),
+				1
+			);
+			climbingholds[hold_nr].GetComponent<Renderer> ().material.color = newColor;
+			yield return null;
+		}
+	}
+
+	/*
+	void renderColor (float[] newColor){
+		Color color2paint = new Color (newColor[0], newColor[1], newColor[2]);
+		climbingholds[0].GetComponent<Renderer>().material.color = color2paint;
+	}
+	*/
+
+	/*
+	void sendDMX(float[] newColor){
+		byte[] DMXData = new byte[512];
+
+		DMXData [0] = (byte)(newColor[0]*255);
+		DMXData [1] = (byte)(newColor[1]*255);
+		DMXData [2] = (byte)(newColor[2]*255);
+
+		ArtEngine.SendDMX (0, DMXData, DMXData.Length);
+	}
+	*/
+}
---
+++ Assets/pickColor.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class pickColor: MonoBehaviour {
+
+	public GameObject cube;
+	public Texture2D heightmap;
+
+	void Start () {
+		Color[] pixels = heightmap.GetPixels (0, 0, heightmap.width, heightmap.height);
+
+
+
+		for (int x = 0; x < heightmap.width; x++) {
+			for (int y = 0; y < heightmap.height; y++) {
+				Color color = pixels[(x*heightmap.width)+ y];
+				GameObject obj = GameObject.CreatePrimitive (PrimitiveType.Cube);
+				obj.transform.position = new Vector3 (x, color.grayscale, y);
+				obj.GetComponent<Renderer> ().material.color = color;
+			}
+		}
+
+		/*
+		if (Input.GetMouseButtonDown(0)) {
+			RaycastHit hit = new RaycastHit ();
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray, out hit)){
+				Debug.Log ("Getroffenes Objekt: " + hit.collider.gameObject.name);
+				Debug.Log ("Klick-Position: " + hit.point);
+				Debug.Log ("Grünwert des ganzen Objekts: " + hit.collider.gameObject.GetComponent<Renderer> ().material.color.g);
+
+				var texture =  GetComponent<Renderer> ().material.mainTexture;
+
+
+				Color co = Color.green;
+				co = tex2d.GetPixel ((int) hit.point.x, (int)hit.point.y);
+				Debug.Log ("Farbe an x/y: " + co);
+
+				cube.GetComponent<Renderer> ().material.color = co;
+
+
+				// Color backgroundcolor = hit.collider.gameObject.GetComponent<Renderer> ().material.color;
+				// cube.GetComponent<Renderer> ().material.color = backgroundcolor;
+
+			}
+		}
+		*/
+	}
+	void Update(){
+	}
+}

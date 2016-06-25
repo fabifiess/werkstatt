22639f309682eb475f1ec04878e90d9afd58765b
Fab F <MXO-GFX@Maximilians-iMac-2.local>
2016-06-25 14:20:14 +0200

    Added OSC receive

---
+++ Assets/Scripts/oscControl.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityOSC;
+
+public class oscControl : MonoBehaviour {
+	
+	private Dictionary<string, ServerLog> servers;
+
+	public GameObject cube;
+
+	public string fader1 = "/1/fader1";
+	public int thisPort = 8000;
+	private string thatIpAddress;
+	private int thatPort;
+
+	void Start() {	
+		thatIpAddress = "127.0.0.1";
+		thatPort = 9000;
+
+		// public void Init(int thisPort, string thatIpAddress, int thatPort)
+		OSCHandler.Instance.Init(thisPort, thatIpAddress, thatPort); //init OSC
+		servers = new Dictionary<string, ServerLog>();
+		cube = GameObject.Find ("Cube");
+	}
+
+	// NOTE: The received messages at each server are updated here
+	void Update() {
+		servers = OSCHandler.Instance.Servers;
+		OSCHandler.Instance.UpdateLogs();
+
+		foreach (KeyValuePair<string, ServerLog> item in servers) {
+			// If we have received at least one packet,
+			// show the last received from the log in the Debug console
+			if (item.Value.log.Count > 0) {
+				int lastPacketIndex = item.Value.packets.Count - 1;
+				Debug.Log ("________________________");
+
+
+				/*
+				UnityEngine.Debug.Log (String.Format ("SERVER: {0} ADDRESS: {1} VALUE : {2}", 
+					                                    item.Key, // Server name
+					                                    item.Value.packets [lastPacketIndex].Address, // OSC address
+					                                    item.Value.packets [lastPacketIndex].Data [0].ToString ())); //First data value
+				*/
+
+
+
+
+				float tempVal = float.Parse (item.Value.packets [lastPacketIndex].Data [0].ToString ());
+				Debug.Log ("Print Fader: "+tempVal);
+				if (item.Value.packets [lastPacketIndex].Address == fader1) {
+					cube.transform.localScale = new Vector3 (tempVal, tempVal, tempVal);
+				} 
+			}
+		}
+	}
+}
